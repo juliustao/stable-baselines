9789d253ebc82b93ba3e20e8a8d638f3b7aea8af
juliustao <juliusltao@gmail.com>
2020-11-30 22:57:51 -0500

    load saved model if it exists

--- fetch.py
+++ fetch.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 import gym
@@ -32,71 +33,110 @@ def train(env_name, model_name):
     env = gym.make(env_name)
 
     log_path = f"../logs/{model_name}"
-    save_path = f"../models/{model_name}"
+    save_path = f"../models/{model_name}"  # this is without the .zip
 
-    # SAC hyperparams
-    if env_name == "FetchReach-v1":
-        # https://github.com/araffin/rl-baselines-zoo/blob/master/trained_agents/her/FetchReach-v1/config.yml
-        model = HER(
-            'MlpPolicy',
-            env,
-            SAC,
-            n_sampled_goal=4,
-            goal_selection_strategy='future',
-            buffer_size=1000000,
-            ent_coef='auto',
-            batch_size=256,
-            gamma=0.95,
-            learning_rate=0.001,
-            learning_starts=1000,
+    if os.path.isfile(save_path + ".zip"):
+        model = HER.load(
+            save_path,
+            env=env,
             verbose=1,
             tensorboard_log=log_path,
         )
-        n_timesteps = 20000
-        #n_timesteps = 25000  # overshoot in case we do not converge in IORT or IOIT
-    elif env_name == "FetchPush-v1":
-        # https://github.com/araffin/rl-baselines-zoo/blob/master/trained_agents/her/FetchPush-v1/config.yml
-        model = HER(
-            'MlpPolicy',
-            env,
-            SAC,
-            n_sampled_goal=4,
-            goal_selection_strategy='future',
-            buffer_size=1000000,
-            ent_coef='auto',
-            batch_size=256,
-            gamma=0.95,
-            learning_rate=0.001,
-            learning_starts=1000,
-            verbose=1,
-            tensorboard_log=log_path,
-        )
-        n_timesteps = int(3e6)
-        #n_timesteps = int(3.5e6)  # overshoot in case we do not converge in IORT or IOIT
-    elif env_name == "FetchPickAndPlace-v1":
-        # https://github.com/araffin/rl-baselines-zoo/blob/master/trained_agents/her/FetchPickAndPlace-v1/config.yml
-        model = HER(
-            'MlpPolicy',
-            env,
-            SAC,
-            n_sampled_goal=4,
-            goal_selection_strategy='future',
-            buffer_size=1000000,
-            ent_coef='auto',
-            gamma=0.95,
-            learning_starts=1000,
-            train_freq=1,
-            verbose=1,
-            tensorboard_log=log_path,
-        )
-        n_timesteps = int(4e6)
-        #n_timesteps = int(5e6)  # overshoot in case we do not converge in IORT or IOIT
-        env = DoneOnSuccessWrapper(env)
+        print("\nLoaded previous saved model\n")
+        n_timesteps = int(1e6)
     else:
-        raise ValueError("Unsupported environment")
+        # SAC hyperparams
+        if env_name == "FetchReach-v1":
+            # https://github.com/araffin/rl-baselines-zoo/blob/master/trained_agents/her/FetchReach-v1/config.yml
+            model = HER(
+                'MlpPolicy',
+                env,
+                SAC,
+                n_sampled_goal=4,
+                goal_selection_strategy='future',
+                buffer_size=1000000,
+                ent_coef='auto',
+                batch_size=256,
+                gamma=0.95,
+                learning_rate=0.001,
+                learning_starts=1000,
+                verbose=1,
+                tensorboard_log=log_path,
+            )
+            #n_timesteps = 20000
+            n_timesteps = 24000  # overshoot in case we do not converge in IORT or IOIT
+        elif env_name == "FetchPush-v1":
+            # https://github.com/araffin/rl-baselines-zoo/blob/master/trained_agents/her/FetchPush-v1/config.yml
+            model = HER(
+                'MlpPolicy',
+                env,
+                SAC,
+                n_sampled_goal=4,
+                goal_selection_strategy='future',
+                buffer_size=1000000,
+                ent_coef='auto',
+                batch_size=256,
+                gamma=0.95,
+                learning_rate=0.001,
+                learning_starts=1000,
+                verbose=1,
+                tensorboard_log=log_path,
+            )
+            #n_timesteps = int(3e6)
+            n_timesteps = int(3.6e6)  # overshoot in case we do not converge in IORT or IOIT
+        # elif env_name == "FetchPush-v1":
+        #     # https://github.com/araffin/rl-baselines-zoo/blob/master/trained_agents/her/FetchPickAndPlace-v1/config.yml
+        #     model = HER(
+        #         'MlpPolicy',
+        #         env,
+        #         SAC,
+        #         n_sampled_goal=4,
+        #         goal_selection_strategy='future',
+        #         buffer_size=1000000,
+        #         ent_coef='auto',
+        #         gamma=0.95,
+        #         learning_starts=1000,
+        #         train_freq=1,
+        #         verbose=1,
+        #         tensorboard_log=log_path,
+        #     )
+        #     #n_timesteps = int(3e6)
+        #     n_timesteps = int(3.6e6)  # overshoot in case we do not converge in IORT or IOIT
+        #     env = DoneOnSuccessWrapper(env)
+        elif env_name == "FetchPickAndPlace-v1":
+            # https://github.com/araffin/rl-baselines-zoo/blob/master/trained_agents/her/FetchPickAndPlace-v1/config.yml
+            model = HER(
+                'MlpPolicy',
+                env,
+                SAC,
+                n_sampled_goal=4,
+                goal_selection_strategy='future',
+                buffer_size=1000000,
+                ent_coef='auto',
+                gamma=0.95,
+                learning_starts=1000,
+                train_freq=1,
+                verbose=1,
+                tensorboard_log=log_path,
+            )
+            #n_timesteps = int(4e6)
+            n_timesteps = int(4.8e6)  # overshoot in case we do not converge in IORT or IOIT
+            env = DoneOnSuccessWrapper(env)
+        else:
+            raise ValueError("Unsupported environment")
 
+    print(f"\nNumber of train timesteps: {n_timesteps}\n")
     model.learn(n_timesteps)
     model.save(save_path)
+
+    # epoch_timesteps = int(5e3)  # 100k timesteps in an epoch
+    # epoch_timesteps = min(epoch_timesteps, n_timesteps)
+    # for step in range(0, n_timesteps, epoch_timesteps):
+    #     model.learn(epoch_timesteps)
+    #     model.save(save_path)
+    #     current_timestep = epoch_timesteps * (step + 1)
+    #     print("\nSaved model at timestep {}\n".format(current_timestep))
+    #     model = HER.load(save_path, env=env)
 
 
 def test(env_name, model_name):
@@ -105,6 +145,11 @@ def test(env_name, model_name):
 
     # Load saved model
     model = HER.load(save_path, env=env)
+
+    # Use DoneOnSuccessWrapper
+    if model_name == "FetchPickAndPlace-v1":
+        print("\nUsing DoneOnSuccessWrapper\n")
+        env = DoneOnSuccessWrapper(env)
 
     obs = env.reset()
 
