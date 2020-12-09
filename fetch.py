09d07d8a0333561687d066665779243cc6e79369
juliustao <juliusltao@gmail.com>
2020-12-08 19:06:06 -0500

    add episode success rate; comments contain video/image code

--- fetch.py
+++ fetch.py
@@ -2,7 +2,9 @@ import os
 import sys
 
 import gym
+#import imageio
 import numpy as np
+#from PIL import Image
 
 from stable_baselines import HER, SAC, DDPG, TD3
 
@@ -32,7 +34,8 @@ class DoneOnSuccessWrapper(gym.Wrapper):
 def make_env(env_name):
     # https://github.com/araffin/rl-baselines-zoo/blob/master/hyperparams/her.yml
     env = gym.make(env_name)
-    if env_name in ["FetchPush-v1", "FetchPickAndPlace-v1"]:
+    if env_name == "FetchPickAndPlace-v1":
+    # if env_name in ["FetchPush-v1", "FetchPickAndPlace-v1"]:
         print("\nUsing DoneOnSuccessWrapper\n")
         env = DoneOnSuccessWrapper(env)
     return env
@@ -77,27 +80,8 @@ def train(env_name, model_name):
             )
             #n_timesteps = 20000
             n_timesteps = 24000  # overshoot in case we do not converge in IORT or IOIT
-        # elif env_name == "FetchPush-v1":
-        #     # https://github.com/araffin/rl-baselines-zoo/blob/master/trained_agents/her/FetchPush-v1/config.yml
-        #     model = HER(
-        #         'MlpPolicy',
-        #         env,
-        #         SAC,
-        #         n_sampled_goal=4,
-        #         goal_selection_strategy='future',
-        #         buffer_size=1000000,
-        #         ent_coef='auto',
-        #         batch_size=256,
-        #         gamma=0.95,
-        #         learning_rate=0.001,
-        #         learning_starts=1000,
-        #         verbose=1,
-        #         tensorboard_log=log_path,
-        #     )
-        #     #n_timesteps = int(3e6)
-        #     n_timesteps = int(3.6e6)  # overshoot in case we do not converge in IORT or IOIT
         elif env_name == "FetchPush-v1":
-            # https://github.com/araffin/rl-baselines-zoo/blob/master/hyperparams/her.yml
+            # https://github.com/araffin/rl-baselines-zoo/blob/master/trained_agents/her/FetchPush-v1/config.yml
             model = HER(
                 'MlpPolicy',
                 env,
@@ -106,14 +90,34 @@ def train(env_name, model_name):
                 goal_selection_strategy='future',
                 buffer_size=1000000,
                 ent_coef='auto',
+                batch_size=256,
                 gamma=0.95,
+                learning_rate=0.001,
                 learning_starts=1000,
-                train_freq=1,
                 verbose=1,
                 tensorboard_log=log_path,
             )
             #n_timesteps = int(3e6)
             n_timesteps = int(3.6e6)  # overshoot in case we do not converge in IORT or IOIT
+        # elif env_name == "FetchPush-v1":
+        #     # https://github.com/araffin/rl-baselines-zoo/blob/master/hyperparams/her.yml
+        #     model = HER(
+        #         'MlpPolicy',
+        #         env,
+        #         SAC,
+        #         n_sampled_goal=4,
+        #         goal_selection_strategy='future',
+        #         buffer_size=1000000,
+        #         ent_coef='auto',
+        #         gamma=0.95,
+        #         learning_starts=1000,
+        #         train_freq=1,
+        #         verbose=1,
+        #         tensorboard_log=log_path,
+        #     )
+        #     #n_timesteps = int(3e6)
+        #     #n_timesteps = int(3.6e6)  # overshoot in case we do not converge in IORT or IOIT
+        #     n_timesteps = int(7.2e6)
         elif env_name == "FetchPickAndPlace-v1":
             # https://github.com/araffin/rl-baselines-zoo/blob/master/trained_agents/her/FetchPickAndPlace-v1/config.yml
             # https://github.com/araffin/rl-baselines-zoo/blob/master/hyperparams/her.yml
@@ -152,26 +156,42 @@ def train(env_name, model_name):
 
 def test(env_name, model_name):
     env = make_env(env_name)
+    #video_path = f"../videos/{model_name}"
     save_path = f"../models/{model_name}"
 
     # Load saved model
     model = HER.load(save_path, env=env)
 
+    #images = []
     obs = env.reset()
+    # img = env.render(mode='rgb_array')
+    # img = np.array(img)
+    # img = Image.fromarray(img)
+    # img.save("push_fixed_initial.png")
 
     # Evaluate the agent
+    num_success_episodes = 0
+    num_episodes = 0
     episode_reward = 0.0
     cumul_steps = 0
-    for _ in range(100):
+    while num_episodes < 100:
+        #images.append(img)
         action, _ = model.predict(obs)
         obs, reward, done, info = env.step(action)
         env.render()
+        #img = env.render(mode='rgb_array')
         episode_reward += reward
         cumul_steps += 1
         if done or info.get('is_success', False):
+            if info.get('is_success', False):
+                num_success_episodes += 1
             print("Reward:{:>6}\tSuccess?{:>4}\tFinalStep:{:>4}".format(episode_reward, info.get('is_success', False), cumul_steps))
+            num_episodes += 1
             episode_reward = 0.0
             obs = env.reset()
+    episode_success_rate = num_success_episodes / num_episodes
+    print(f"Episode success rate: {episode_success_rate}")
+    #imageio.mimsave(os.path.abspath(save_path))
 
 
 if __name__ == "__main__":
